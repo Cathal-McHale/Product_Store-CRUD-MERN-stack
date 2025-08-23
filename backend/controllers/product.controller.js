dcad9603277c5564c7b427054c20cb898348d155
Cathal McHale <cm@tus.ie>
2025-08-23 15:34:38 +0100

    simplified API routes in product controller

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,67 @@
+import Product from "../models/product.model.js";
+import mongoose from "mongoose";
+import express from "express";
+
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const createProduct = async (req, res) => {
+  const product = req.body;
+
+  if (
+    !product.name ||
+    !product.price ||
+    !product.image ||
+    !product.description
+  ) {
+    return res.status(400).json({ message: "Please fill all the fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const updates = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ message: "Invalid product ID" });
+  }
+
+  try {
+    const product = await Product.findByIdAndUpdate(id, updates, { new: true });
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
