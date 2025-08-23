2a1fef30e36078af11cdfd7f26606b57d0b9c80c
Cathal McHale <cm@tus.ie>
2025-08-23 13:37:56 +0100

    Inital setup

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+//console.log(process.env.MONGO_URI);
+
+app.listen(5000, () => {
+    connectDB();
+  console.log('Server is running on at http://localhost:5000');
+});
