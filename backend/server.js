ffa2d8631ef458a0efa0d2085cd927bd8f88ede1
Cathal McHale <cm@tus.ie>
2025-09-02 16:08:54 +0100

    set up for hosting

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from "path";
 import { connectDB } from './config/db.js';
 import Product from "./models/product.model.js";
 import mongoose from 'mongoose';
@@ -9,10 +10,20 @@ dotenv.config();
 const PORT = process.env.PORT = 5000;
 const app = express();
 
+const __dirname = path.resolve();
 app.use(express.json());//accept JSON data in body
 app.use("/api/products", productRoutes);
 
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get(/^\/.*$/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+
+}
+
 app.listen(5000, () => {
     connectDB();
-  console.log('Server is running on at' + PORT);
+  console.log('Server is running on at http://localhost:' + PORT);
 });
